6b16c710861770270c698e4def7673756cb4ccdd
BennySP2022 <benny.susilo.putro@outlook.com>
2022-06-02 13:37:27 +0700

    update3

--- lib/view/main/latihan_soal/paket_soal_page.dart
+++ lib/view/main/latihan_soal/paket_soal_page.dart
@@ -18,38 +18,79 @@ class _PaketSoalPageState extends State<PaketSoalPage> {
     return Scaffold(
       backgroundColor: R.colors.grey,
       appBar: AppBar(
-        title: Text("Paket Soal"),
+        title: Text(
+          "Paket Soal",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
       ),
-      body: GridView.count(
-        mainAxisSpacing: 10,
-        crossAxisSpacing: 10,
-        crossAxisCount: 2,
-        childAspectRatio: 4 / 3,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Pilih Paket Soal",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                )),
+            Expanded(
+              child: GridView.count(
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 2,
+                children: [
+                  PaketSoalWidget(),
+                  PaketSoalWidget(),
+                  PaketSoalWidget(),
+                  PaketSoalWidget(),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container PaketSoalWidget() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+      ),
+      // margin: EdgeInsets.all(13),
+      padding: EdgeInsets.all(13),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Container(
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
-              color: Colors.white,
+              color: Colors.blue.withOpacity(0.2),
             ),
-            margin: EdgeInsets.all(13),
-            padding: EdgeInsets.all(13),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.blue.withOpacity(0.2),
-                  ),
-                  padding: EdgeInsets.all(12),
-                  child: Image.asset(
-                    R.assets.icNote,
-                    width: 14,
-                  ),
-                ),
-                Text("Aljabar"),
-                Text("070 Paket Soal"),
-              ],
+            padding: EdgeInsets.all(12),
+            child: Image.asset(
+              R.assets.icNote,
+              width: 14,
+            ),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          Text(
+            "Aljabar",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          Text(
+            "070 Paket Soal",
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 9,
+              color: R.colors.greySubtitle,
             ),
           ),
         ],
