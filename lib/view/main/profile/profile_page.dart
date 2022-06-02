c784ced8d42ba2c32161d4294a174b5b5d69aeb6
BennySP2022 <benny.susilo.putro@outlook.com>
2022-06-02 20:18:25 +0700

    ProfilePage

--- lib/view/main/profile/profile_page.dart
+++ lib/view/main/profile/profile_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../../../R/r.dart';
+
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
@@ -11,10 +13,140 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Text("Akun Saya"),
+        centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "Edit",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
+      ),
       body: Column(
         children: [
-          Text("PROFILE"),
+          Container(
+            padding: EdgeInsets.only(
+              top: 20,
+              bottom: 60,
+              right: 15,
+              left: 15,
+            ),
+            decoration: BoxDecoration(
+              color: R.colors.primary,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(9),
+                bottomRight: Radius.circular(9),
+              ),
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Nama User",
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      Text(
+                        "Nama Sekolah User",
+                        style: TextStyle(
+                          fontSize: 12,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Image.asset(
+                  R.assets.imgUser,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 25,
+          ),
+          Container(
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 7,
+                  color: Colors.black.withOpacity(0.25),
+                ),
+              ],
+            ),
+            margin: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+            padding: EdgeInsets.symmetric(vertical: 16, horizontal: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Indentitas Diri "),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Nama Lengkap",
+                  style: TextStyle(
+                    color: R.colors.greySubtitle,
+                    fontSize: 12,
+                  ),
+                ),
+                Text(
+                  "Nama Lengkap User",
+                  style: TextStyle(
+                    color: R.colors.greySubtitle,
+                    fontSize: 13,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 18, horizontal: 13),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: [
+                BoxShadow(
+                  blurRadius: 7,
+                  color: Colors.black.withOpacity(0.25),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.exit_to_app,
+                  color: Colors.red,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Title",
+                  style: TextStyle(
+                    color: Colors.red,
+                    fontSize: 12,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
